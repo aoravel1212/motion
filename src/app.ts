c893af163ed64c552ec4797e4a836fe5da566d1b
slevme <hby2233@naver.com>
2023-01-05 21:02:05 +0900

    add PageComponent & app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,10 @@
+import { PageComponent } from "./components/page.js";
 class App {
-  constructor() {}
+  private readonly page: PageComponent;
+  constructor(appRoot: HTMLElement) {
+    this.page = new PageComponent();
+    this.page.attachTo(appRoot);
+  }
 }
+
+new App(document.querySelector(".document")! as HTMLElement);
