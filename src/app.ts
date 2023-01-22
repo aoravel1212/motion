ede82f67151ff0704229f52517499ffff65d5d57
slevme <hby2233@naver.com>
2023-01-22 22:58:49 +0900

    add PageItemComponent

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,26 @@
+import { Component } from "./components/component";
 import { NoteComponenet } from "./components/page/item/note.js";
 import { TodoComponenet } from "./components/page/item/todo.js";
 import { ImageComponenet } from "./components/page/item/image.js";
-import { PageComponent } from "./components/page/page.js";
+import { Composable, PageComponent } from "./components/page/page.js";
 import { VideoComponenet } from "./components/page/item/video.js";
 class App {
-  private readonly page: PageComponent;
+  private readonly page: Component & Composable;
   constructor(appRoot: HTMLElement) {
     this.page = new PageComponent();
     this.page.attachTo(appRoot);
 
-    const image = new ImageComponenet(
-      "Image Title",
-      "https://picsum.photos/600/300"
-    );
-    image.attachTo(appRoot, "beforeend");
+    const image = new ImageComponenet("Image Title", "https://picsum.photos/600/300");
+    this.page.addChild(image);
 
     const note = new NoteComponenet("Note Title", "Note Body");
-    note.attachTo(appRoot, "beforeend");
+    this.page.addChild(note);
 
     const todo = new TodoComponenet("Todo Title", "Todo Item");
-    todo.attachTo(appRoot, "beforeend");
+    this.page.addChild(todo);
 
-    const video = new VideoComponenet(
-      "Video Title",
-      "https://www.youtube.com/embed/0qvSCmcMUc0"
-    );
-    video.attachTo(appRoot, "beforeend");
+    const video = new VideoComponenet("Video Title", "https://www.youtube.com/embed/0qvSCmcMUc0");
+    this.page.addChild(video);
   }
 }
 
