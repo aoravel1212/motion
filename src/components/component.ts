cc2414a029421ab32c8533e6632e74a32bf91754
slevme <hby2233@naver.com>
2023-01-24 14:55:17 +0900

    add component removal function

--- src/components/component.ts
+++ src/components/component.ts
@@ -1,5 +1,6 @@
 export interface Component {
   attachTo(parent: HTMLElement, position?: InsertPosition): void;
+  removeFrom(parent: HTMLElement): void;
 }
 
 // html 요소를 만드는 것을 캡슐화 하는 클래스
@@ -10,7 +11,15 @@ export class BaseComponent<T extends HTMLElement> implements Component {
     template.innerHTML = htmlString;
     this.element = template.content.firstElementChild! as T;
   }
+
   attachTo(parent: HTMLElement, position: InsertPosition = "afterbegin") {
     parent.insertAdjacentElement(position, this.element);
+  }
+
+  removeFrom(parent: HTMLElement) {
+    if (parent !== this.element.parentElement) {
+      throw new Error("Parent mismatch!");
+    }
+    parent.removeChild(this.element);
   }
 }
--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -4,7 +4,11 @@ export interface Composable {
   addChild(child: Component): void;
 }
 
+// 아무런 인자도 받지 않고 반환하지도 않음. 그냥 닫혔다는라는 상태를 알려주기만 하는 콜백함수.
+type OnCloseListener = () => void;
 class PageItemComponent extends BaseComponent<HTMLElement> implements Composable {
+  // 외부로부터 전달받은 콜백함수를 저장하고 있을 변수. 전달받지 않을 경우 undefined가 될 수 있도록 옵셔널로 설정.
+  private closeLisener?: OnCloseListener;
   constructor() {
     super(`<li class="page-item">
             <section class="page-item__body"></section>
@@ -12,10 +16,18 @@ class PageItemComponent extends BaseComponent<HTMLElement> implements Composable
               <button class="close">&times;</button>
             </div>
           </li>`);
+    const closeBtn = this.element.querySelector(".close")! as HTMLButtonElement;
+    closeBtn.onclick = () => {
+      // 클로즈 버튼이 클릭 되면 closeLisener가 있는지 확인하고, 있는 경우 closeLisener를 호출한다.
+      this.closeLisener && this.closeLisener();
+    };
   }
   addChild(child: Component) {
     const container = this.element.querySelector(".page-item__body")! as HTMLElement;
     child.attachTo(container);
+  }
+  setOnCloseListener(listener: OnCloseListener) {
+    this.closeLisener = listener;
   }
 }
 
@@ -28,5 +40,8 @@ export class PageComponent extends BaseComponent<HTMLUListElement> implements Co
     const item = new PageItemComponent();
     item.addChild(section);
     item.attachTo(this.element, "beforeend");
+    item.setOnCloseListener(() => {
+      item.removeFrom(this.element);
+    });
   }
 }
